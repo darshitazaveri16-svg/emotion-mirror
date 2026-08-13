acfe67050805e75a37605df3e9e1f0eca6b2defd
Khushi <ktm407301@gmail.com>
2026-08-13 22:26:15 +0530

    feat: express server setup with MongoDB Atlas connection

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    status: 'Emotion Mirror backend is running'
+  });
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
